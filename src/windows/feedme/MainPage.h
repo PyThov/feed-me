127f5bd8723507a71a515dff6428b8626b32c107
PyThov <noahthovson@gmail.com>
2022-05-26 20:26:53 -0500

    init project

---
+++ src/windows/feedme/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::feedme::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::feedme::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
